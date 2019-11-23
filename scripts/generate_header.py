707ace47b80531f7268bfa9863466357dd334d98
Christopher Stillson <38892712+sistillson@users.noreply.github.com>
2019-11-22 16:01:42 -0800

    Extend generate headers to work with oddly sized interrupt fields (#16)
    * Extend generate headers to work with oddly sized interrupt fields
    add interupt information
    
    * updated to be more discriminating about object that the interupts are
    pulled from
    
    * cleaned up to work with multiple devices
    
    * Changes from richard
    
    * typo in comment

--- scripts/generate_header.py
+++ scripts/generate_header.py
@@ -8,9 +8,14 @@ import textwrap
 import typing as t
 from dataclasses import dataclass
 from pathlib import Path
+from collections import Counter
 
 PlainJSONType = t.Union[dict, list, t.AnyStr, float, bool]
 JSONType = t.Union[PlainJSONType, t.Iterator[PlainJSONType]]
+
+NAME_COLLISION_DICT = Counter()
+
+# Json utility
 
 
 def walk(j_obj: JSONType) -> t.Iterator[JSONType]:
@@ -31,102 +36,78 @@ def walk(j_obj: JSONType) -> t.Iterator[JSONType]:
             yield from walk(j)
 
 
+# Data Classes
+# we pull RegisterFields, Interrupts, and Devices from the Object Model.
+# These are the data classes we use to represent them
+
 @dataclass(frozen=True)
-class Register:
+class RegisterField:
     """
-    Description of memory-mapped control register within a device
+    data class to hold information about a register field.
     """
     name: str
-    offset: int  # in bytes
+    offset: int  # in bits
     width: int  # in bits
+    regFieldGroup: str
+    all_registers: t.ClassVar = {}
 
     @staticmethod
-    def make_register(name: str, offset: int, width: int) -> "Register":
-        if width not in (8, 16, 32, 64):
-            raise Exception(f'Invalid register width {width}, for register '
-                            f'{name}.\n'
-                            f'Width should be not 8, 16, 32, or 64.\n'
-                            f'Please fix the register width in DUH document.')
-        return Register(name, offset, width)
+    def make_register(name: str, offset: int, width: int, group: str) \
+            -> "RegisterField":
+        key = f'{name}-{group}'
+        if name != 'reserved' and key in RegisterField.all_registers:
+            register_field = RegisterField(name, offset, width, group)
+            if RegisterField.all_registers[key] != register_field:
+                raise Exception(f'Non matching register fields {key}')
+            else:
+                return RegisterField.all_registers[key]
 
+        RegisterField.all_registers[key] = RegisterField(name, offset, width, group)
+        return RegisterField.all_registers[key]
+
+
+@dataclass(frozen=True)
+class Interrupt:
+    """
+    Data class to hold information about an interrupt. May be
+    unnamed, in which case the name is and empty string.
+    """
+    number: int
+    name: str
+    all_interrupts: t.ClassVar = {}
+
+    @staticmethod
+    def make_interrupt(number, name=''):
+        if name and name in Interrupt.all_interrupts:
+            an_interrupt = Interrupt(number, name)
+            if Interrupt.all_interrupts[name] != an_interrupt:
+                raise Exception(f"duplicate interrupt {name}")
+            else:
+                return Interrupt.all_interrupts[name]
+
+        Interrupt.all_interrupts[name] = Interrupt(number, name)
+        return Interrupt.all_interrupts[name]
+
+
+@dataclass(frozen=True)
+class DeviceBase:
+    """
+    Data class to hold information about a device on the SOC. Include all
+    register fields and interrupts for the device.
+    """
+    name: str
+    index: int
+    base_interrupt: int
+    base_address: int
+    interrupts: t.List[Interrupt]
+    registers: t.List[RegisterField]
 
 ###
 # templates
 ###
 
-def generate_vtable_declarations(device_name: str,
-                                 reg_list: t.List[Register]) -> str:
-    """
-    Generate the vtable entries for a device and set of registers. This
-    creates the declarations for function pointers for all the driver functions.
-    This is used to provide a single point for all functions that can be used
-    for multiple devices.
-    :param device_name: the name of the device
-    :param reg_list: a list of Register objects for the device
-    :return: the c code for the vtable entries
-    """
+# This is the base template for the header we generate.
 
-    rv = []
-
-    for a_reg in reg_list:
-        reg_name = a_reg.name.lower()
-        size = a_reg.width
-
-        write_func = f'    void (*v_{device_name}_{reg_name}_write)(uint32_t * {device_name}_base, uint{size}_t data);'
-        read_func = f'    uint{size}_t (*v_{device_name}_{reg_name}_read)(uint32_t  *{device_name}_base);'
-
-        rv.append(write_func)
-        rv.append(read_func)
-
-    return '\n'.join(rv)
-
-
-def generate_metal_vtable_definition(devices_name: str) -> str:
-    """
-    Generate the vtable and base address variable definitions
-    for the given device name
-
-    :param devices_name:
-    :return: The c code for the metal device
-    """
-
-    return f'    uint32_t *{devices_name}_base;\n' + \
-           f'    const struct metal_{devices_name}_vtable vtable;'
-
-
-def generate_protos(device_name: str, reg_list: t.List[Register]) -> str:
-    """
-    Generate the function prototypes for a given device and register list.
-
-    :param device_name: The device name
-    :param reg_list: the list of registers for the device
-    :return: the c language prototypes for the device
-    """
-
-    rv = []
-
-    dev_struct = f'const struct metal_{device_name} *{device_name}'
-
-    for a_reg in reg_list:
-        reg_name = a_reg.name.lower()
-        size = a_reg.width
-
-        write_func = f'void metal_{device_name}_{reg_name}_write({dev_struct}, uint{size}_t data);'
-        read_func = f'uint{size}_t metal_{device_name}_{reg_name}_read({dev_struct});'
-
-        rv.append(write_func)
-        rv.append(read_func)
-
-    get_device = f'const struct metal_{device_name} *get_metal_{device_name}' \
-                 f'(uint8_t index);'
-    rv.append(get_device)
-
-    return '\n'.join(rv)
-
-
-###
-# templates
-###
 
 METAL_BASE_HDR_TMPL = \
     """
@@ -135,79 +116,230 @@ METAL_BASE_HDR_TMPL = \
 
     #ifndef ${vendor}_${device}_h
     #define ${vendor}_${device}_h
- 
-    // To use ${cap_device}_BASES, use it as the
-    // initializer to an array of ints, i.e.
-    // int bases[] = ${cap_device}_BASES;
-
-    #define ${cap_device}_COUNT ${dev_count} 
-    #define ${cap_device}_BASES {${base_address}}
     
+    #define ${capitalized_device}_COUNT ${dev_count}
+
+    // To use ${capitalized_device}_INTERRUPT_BASES, use it as the
+    // initializer to an array of ints, i.e.
+    // int interrupt_bases[${capitalized_device}_COUNT] = ${capitalized_device}_INTERRUPT_BASES;
+    // there are ${capitalized_device}_INTERRUPT_COUNT interrupts per
+    // device.
+    
+    ${interrupts}
+
+    // To use ${capitalized_device}_BASES, use it as the
+    // initializer to an array of ints, i.e.
+    // int bases[${capitalized_device}_COUNT] = ${capitalized_device}_BASES;
+
+    #define ${capitalized_device}_BASES {${base_address}}
 
     // : these macros have control_base as a hidden input
-    #define METAL_${cap_device}_REG(offset) (((unsigned long)control_base + offset))
-    #define METAL_${cap_device}_REGW(offset) \\
-       (__METAL_ACCESS_ONCE((__metal_io_u32 *)METAL_${cap_device}_REG(offset)))
+    // use with the _BYTE #define's
+    #define METAL_${capitalized_device}_REG(offset) ((unsigned long)control_base + (offset))
+    #define METAL_${capitalized_device}_REGW(offset) \\
+       (__METAL_ACCESS_ONCE((__metal_io_u32 *)METAL_${capitalized_device}_REG(offset)))
+
+    #define METAL_${capitalized_device}_REGBW(offset) \\
+       (__METAL_ACCESS_ONCE((uint8_t *)METAL_${capitalized_device}_REG(offset)))
+
+    // METAL_NAME => bit offset from base
+    // METAL_NAME_BYTE => (uint8_t *) offset from base
+    // METAL_NAME_BIT => number of bits into METAL_NAME_BYTE
+    // METAL_NAME_WIDTH => bit width
 
     ${register_offsets}
-    
-    ${register_widths}
 
     #endif
     """
 
 
-def generate_offsets(device_name: str, reg_list: t.List[Register]) -> str:
+# sub templates
+# generate sub parts of template
+def generate_offsets(device_name: str, dev_list: t.List[DeviceBase]) -> str:
     """
     Generate the register offset macros
 
     :param device_name: the name of the device
-    :param reg_list: the list of registers for the device.
+    :param dev_list: the list of devices for the SOC
     :return:The offset c macros for the device and registers
     """
     rv: t.List[str] = []
 
-    cap_device = device_name.upper()
-    for a_reg in reg_list:
-        name = a_reg.name.upper()
-        offset = a_reg.offset
-        macro_line = f'#define METAL_{cap_device}_{name} {offset}'
-        rv.append(macro_line)
+    capitalized_device = device_name.upper()
+    if dev_list:
+        # only need to check the first device
+        for a_reg in dev_list[0].registers:
+            if a_reg.name == 'reserved':
+                continue
+            name = a_reg.name.upper().strip().replace(" ", "")
+            group = a_reg.regFieldGroup.upper().strip().replace(" ", "")
+            offset = a_reg.offset
+            width = a_reg.width
+
+            if name.startswith(group.split('_')[-1] + "_"):
+                group = "_".join(group.split('_')[:-1])
+
+            infix = ''
+            if group is not "":
+                infix = f'_{group}'
+
+            prefix = f'{capitalized_device}_REGISTER{infix}_{name}'
+
+            NAME_COLLISION_DICT[prefix] += 1
+            macro_line =  f'#define {prefix} {offset}\n'
+            macro_line += f'#define {prefix}_BYTE {offset >> 3}\n'
+            macro_line += f'#define {prefix}_BIT {offset & 0x7}\n'
+            macro_line += f'#define {prefix}_WIDTH {width}\n'
+
+            rv.append(macro_line)
 
     return '\n'.join(rv)
 
 
-def generate_widths(device_name: str, reg_list: t.List[Register]) -> str:
-    rv: t.List[str] = []
+def generate_interrupt_defines(bases: t.List[DeviceBase],
+                               device: str) -> str:
+    """
+    generate interrupt sec of file
 
-    cap_device = device_name.upper()
-    for a_reg in reg_list:
-        name = a_reg.name.upper()
-        width = a_reg.width
-        macro_line = f'#define METAL_{cap_device}_{name}_WIDTH {width}'
-        rv.append(macro_line)
+    :param bases: list of devices
+    :param device: the name of the device
+    :return: the interrupt section of the header file.
+    """
+    rv = []
+    dev = device.upper().replace(' ', '')
+
+    if bases[0].interrupts:
+        generic_interrupts = bases[0].interrupts
+        int_base = "#define ABSOLUTE_INTERRUPT(base, relative) ((base) + (relative))"
+
+        int_bases = ','.join(str(i.base_interrupt) for i in bases)
+
+        rv.append(textwrap.dedent(int_base))
+        rv.append(f'#define {dev}_INTERRUPT_BASES {{ {int_bases} }}')
+        rv.append(f'#define {dev}_INTERRUPT_COUNT {len(generic_interrupts)}\n')
+
+        interrupts = []
+        if bases:
+            for an_interrupt in bases[0].interrupts:
+                number = an_interrupt.number - bases[0].base_interrupt
+                if an_interrupt.name:
+                    name = an_interrupt.name.upper().replace(' ', '')
+                    rv.append(f'#define {dev}_INTERRUPT_OFFSET_{name} {number}')
+                interrupts.append(an_interrupt.number)
 
     return '\n'.join(rv)
 
 
 def generate_base_hdr(vendor: str,
                       device: str,
-                      base_addresses: t.List[int],
-                      reglist: t.List[Register]):
+                      devlist: t.List[DeviceBase]):
+    """
+    Master function to generate the include file.
+
+    :param vendor:  string of the vendor name
+    :param device:  string of the device name
+    :param devlist: list of devices
+    :return: a string for the header file
+    """
     template = string.Template(textwrap.dedent(METAL_BASE_HDR_TMPL))
 
-    base = ", ".join(map(str, map(hex, base_addresses)))
+    base = ", ".join(hex(i.base_address) for i in devlist)
+    interrupts = generate_interrupt_defines(devlist, device)
 
     return template.substitute(
         base_address=base,
-        dev_count=len(base_addresses),
+        dev_count=len(devlist),
         vendor=vendor,
         device=device,
-        cap_device=device.upper(),
-        register_offsets=generate_offsets(device, reglist),
-        register_widths=generate_widths(device, reglist)
+        capitalized_device=device.upper(),
+        register_offsets=generate_offsets(device, devlist),
+        interrupts=interrupts,
     )
 
+
+# parsing the OM file
+
+def find_interrupts(object_model: JSONType, device: str) \
+        -> t.List[Interrupt]:
+    """
+    given a parsed device, return the interrupts for the device
+
+    :param object_model: a device parsed from the object model
+    :param device: a string name of the device
+    :return: a list of the interrupts
+    """
+
+    def type_match(dev: str, types: t.List[str]):
+        d_str = dev.lower()
+        for a_type in types:
+            if a_type.lower().endswith(d_str):
+                return True
+        return False
+
+    p = walk(object_model)
+    p = filter(lambda x: '_types' in x, p)
+    p = filter(lambda x: type_match(device, x['_types']), p)
+    p = list(p)
+
+    for dev_om in p:
+        p = walk(dev_om)
+        p = filter(lambda x: '_types' in x, p)
+        p = filter(lambda x: 'OMInterrupt' in x['_types'], p)
+
+    rv = []
+    for an_interrupt in p:
+        number = an_interrupt['numberAtReceiver']
+        name = an_interrupt.get('name', '')
+        if '@' in name:
+            name = ''
+        int_data = Interrupt.make_interrupt(number, name)
+        rv.append(int_data)
+
+    return rv
+
+
+def find_registers(object_model: JSONType) -> t.List[RegisterField]:
+    """
+    given a parsed device, return the register fields for the device
+
+    :param object_model: a device parsed from the object model
+    :return: a list of register fields
+    """
+    reglist: t.List[RegisterField] = []
+    for mr in object_model['memoryRegions']:
+        # get base address for each memory region
+        if len(mr['addressSets']) != 1:
+            raise Exception("Can't handle multiple addressSets in a "
+                            "region")
+
+        # get regs for every memory region
+        for aReg in mr['registerMap']['registerFields']:
+            r_name = aReg['description']['name']
+            r_group = aReg['description']['group']
+            r_offset = aReg['bitRange']['base']
+            r_width = aReg['bitRange']['size']
+            r = RegisterField.make_register(r_name,
+                                            r_offset,
+                                            r_width,
+                                            r_group)
+
+            reglist.append(r)
+
+    return reglist
+
+
+def find_devices(object_model: JSONType,
+                 device: str) -> JSONType:
+    """
+
+    :param object_model: The full object model for the soc
+    :param device: the name of the device in question
+    :return: a list of the devices in the soc
+    """
+    p = walk(object_model)
+    p = filter(lambda x: '_types' in x, p)
+    p = filter(lambda x: f'OM{device}' in x['_types'], p)
+    return list(enumerate(p))
 
 ###
 # main
@@ -270,42 +402,39 @@ def main() -> int:
     # parse OM to find base address of all devices
     # ###
 
-    p = walk(object_model)
-    p = filter(lambda x: '_types' in x, p)
-    p = filter(lambda x: f'OM{device}' in x['_types'], p)
+    devlist: t.List[DeviceBase] = []
 
-    reglist: t.List[Register] = []
-    bases = []
-    for index, dev in enumerate(p):
-        for m_idx, mr in enumerate(dev['memoryRegions']):
-            # get base address for each memory region
+    devices_om = find_devices(object_model, device)
 
-            if len(mr['addressSets']) == 1:
-                bases.append(mr['addressSets'][0]['base'])
-            else:
-                raise Exception("Can't handle multiple addressSets in a "
-                                "region")
+    for index, dev_om in devices_om:
+        reglist = find_registers(dev_om)
+        intlist = find_interrupts(dev_om, device)
+        base_int = min(i.number for i in intlist)
+        base_address = dev_om['memoryRegions'][0]['addressSets'][0]['base']
 
-            # get regs for every memory region
-            for aReg in mr['registerMap']['registerFields']:
-                r_name = aReg['description']['group']
-                r_offset = aReg['bitRange']['base'] // 8
-                r_width = aReg['bitRange']['size']
-                r = Register.make_register(r_name,
-                                           r_offset,
-                                           r_width)
-                reglist.append(r)
+        devlist.append(DeviceBase(name=device,
+                                  index=index,
+                                  base_interrupt=base_int,
+                                  base_address=base_address,
+                                  interrupts=intlist,
+                                  registers=reglist))
 
     base_hdr_path = bsp_dir_path / f'bsp_{device}'
     base_hdr_path.mkdir(exist_ok=True, parents=True)
     base_header_file_path = base_hdr_path / f'{vendor}_{device}.h'
+
     if overwrite_existing or not base_header_file_path.exists():
         base_header_file_path.write_text(
-            generate_base_hdr(vendor, device, bases, reglist)
-        )
+            generate_base_hdr(vendor,
+                              device,
+                              devlist))
     else:
         print(f"{str(base_header_file_path)} exists, not creating.",
               file=sys.stderr)
+
+    for k, v in NAME_COLLISION_DICT.items():
+        if v > 1:
+            print(f'Variable {k} repeated', file=sys.stderr)
 
     return 0
 
